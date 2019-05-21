223623b019e89cf5db8cd47db769cedb1b420dbe
Alexis P <chpaqou@gmail.com>
2019-05-21 10:25:10 +0200

    Commit du 19-21-05 19:24
    BlocNotes terminé (quelques améliorations à ajouter)

--- BlocNotes/ViewController.swift
+++ BlocNotes/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ui_ChampTexte: UITextField!
+    @IBOutlet weak var ui_TextBlocNote: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
 
+    @IBAction func AddText(_ sender: Any) {
+        if let temp_bloc = ui_TextBlocNote.text, let temp_champ = ui_ChampTexte.text {
+    
+            //Afficher le contenu de  temp_champ dans le ui_TextBlocNote
+            
+            ui_TextBlocNote.text = temp_bloc + " " + temp_champ
+            
+            ui_ChampTexte.text?.removeAll()
+        }
+    }
 }
 
